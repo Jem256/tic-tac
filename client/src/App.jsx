2dec0c84b90a3ee0199e168fb0b0a243f2cd4fcb
Jemimah Nagasha <jemynagasha@gmail.com>
2023-07-04 21:21:35 +0300

    connect signup to server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,34 @@
 import './App.css';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import { StreamChat } from 'stream-chat';
+import Cookies from 'universal-cookie';
 
 function App() {
+    const api_key = import.meta.env.VITE_API_KEY;
+    const cookies = new Cookies();
+    const token = cookies.get('token');
+
+    // const api_secret = process.env.API_SECRECT;
+
+    const client = StreamChat.getInstance(api_key);
+
+    if (token) {
+        client
+            .connectUser(
+                {
+                    id: cookies.get('userId'),
+                    name: cookies.get('username'),
+                    firstName: cookies.get('firstName'),
+                    lastName: cookies.get('lastName'),
+                    hashedPassword: cookies.get('hashedPassword'),
+                },
+                token
+            )
+            .then((user) => {
+                console.log(user);
+            });
+    }
     return (
         <>
             <SignUp />
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,9 +1,33 @@
 import { useState } from 'react';
+import Axios from 'axios';
+import Cookies from 'universal-cookie';
 
 function SignUp() {
+    const cookies = new Cookies();
     const [user, setUser] = useState(null);
 
-    const signUp = () => {};
+    const signUp = () => {
+        Axios.post('http://localhost:3001/signup', user).then((res) => {
+            const {
+                token,
+                userId,
+                firstName,
+                lastName,
+                username,
+                password,
+                hashedPassword,
+            } = res.data;
+
+            cookies.set('token', token);
+            cookies.set('userId', userId);
+            cookies.set('firstName', firstName);
+            cookies.set('lastName', lastName);
+            cookies.set('username', username);
+            cookies.set('password', password);
+            cookies.set('hashedPassword', hashedPassword);
+        });
+    };
+
     return (
         <form className='signUp'>
             <label>Sign Up</label>
