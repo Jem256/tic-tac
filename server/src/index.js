9002d1cb1e5e26526887cf9a39302cdef78bdfe6
Jemimah Nagasha <jemynagasha@gmail.com>
2023-07-06 16:25:29 +0300

    login component

--- server/src/index.js
+++ server/src/index.js
@@ -8,8 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const api_key = process.env.API_KEY;
-const api_secret = process.env.API_SECRECT;
+const api_key = 'tjme3f9w96xc';
+const api_secret =
+    'eenkevwe4yj2bgenwtuh6hpstbppj43qjattsvbjad3dsrmturexudamkq4apd2t';
 
 const serverClient = new StreamChat.getInstance(api_key, api_secret);
 
@@ -33,7 +34,31 @@ app.post('/signup', async (req, res) => {
     }
 });
 
-app.post('/login');
+app.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const { users } = await serverClient.queryUsers({ name: username });
+        if (users.length === 0) return res.json({ message: 'User not found ' });
+
+        const token = serverClient.createToken(userId);
+        const passwordMatch = await bcrypt.compare(
+            password,
+            users[0].hashedPassword
+        );
+
+        if (passwordMatch) {
+            res.json({
+                token,
+                firstName: users[0].firstName,
+                lastName: users[0].lastName,
+                username,
+                userId: users[0].id,
+            });
+        }
+    } catch (error) {
+        res.json(error);
+    }
+});
 
 app.listen(3001, () => {
     console.log('Server running at 3001');
